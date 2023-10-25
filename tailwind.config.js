6f929fbe082bf9293b7e81a8b9d09754eb65d015
Sye-5pace <owusilasyp00@gmail.com>
2023-10-25 16:22:50 +0100

    :lipstick: Build: Tailwindcss config integrate

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function({addVariant}){
+      addVariant('children','&>*')
+    })
+  ],
+}
